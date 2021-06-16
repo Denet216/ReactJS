718732614383039badcc4216ef21be25346b0b82
Denet216 <denet.261@yandex.ru>
2021-06-16 10:00:05 +0300

    NewState

---
+++ src/App copy.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/768px-LEGO_logo.svg.png' alt='qwe' />
+      </header>
+      <nav className='nav'>
+        <div>
+        <a href='#'>Profile</a>
+        </div>
+        <div>
+          <a href='#'>Messages</a>
+          </div>
+         <div>
+            <a href='#'>Main Content111</a>
+            </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://static.mir-kubikov.ru/upload/iblock/796/796e50f8891669bd30a9c39cb6ad4269.jpg' alt=''/>
+      </div>
+      <div>
+      ava + discription
+      </div>
+      <div>
+        My post
+        <div>new post1212</div>
+        <div>Post 11211</div>
+        <div>Post 21</div>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
